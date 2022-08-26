e5696205a9c422deb4c21c18c02d273d0d831bb2
Consue <c.anguitasalinas@gmail.com>
2022-08-25 21:49:33 -0400

    feat: Container component added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css';
 import Input from './components/input-component/Input';
 import Button from './components/button/Button';
 import { useState } from 'react';
+import Container from './components/container/Container';
 
 function App() {
   const [name, setName] = useState('');
@@ -9,12 +10,19 @@ function App() {
 
   return (
     <div className="App">
-      <Input setName={setName} setPassword={setPassword} name={name} />
-      {name !='' &&
-        <div className='hiddenButton'>
-          {password === '252525' ? <Button /> : null}
+      <Container>
+        <div className='section'>
+          <div className='title'>
+            <h1>Estado de los componentes y eventos </h1>
+          </div>
+          <Input setName={setName} setPassword={setPassword} name={name} />
+          {name != '' &&
+            <div className='hiddenButton'>
+              {password === '252525' ? <Button /> : null}
+            </div>
+          }
         </div>
-      }
+      </Container>
     </div>
   )
 }
